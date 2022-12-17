7bf33cc6d3e4da5f8862ea317845466cf38b9c7f
siddharth gandhi <siddharthgandhi@outlook.com>
2022-12-17 16:37:28 +0530

    [Siddharth]Add: Delete a contact

--- AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBookMain.cs
@@ -10,65 +10,83 @@ namespace AddressBook
     {
         List<Contact> address = new List<Contact>();
         public Contact contact = new Contact();
+        public void Display()
+        {
+            Console.WriteLine("\nFirst Name - " + contact.FirstName + "\nLast Name - " + contact.LastName + "\nAddress - " + contact.Address +
+                "\nCity Name - " + contact.City + "\nState Name - " + contact.State + "\nZip Code - " + contact.Zip +
+                "\nPhone Number - " + contact.PhoneNumber + "\nEmail ID - " + contact.Email);
+        }
         public void Create()
         {
+            Console.WriteLine("Enter your First Name");
             contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter your Last Name");
             contact.LastName = Console.ReadLine();
+            Console.WriteLine("Enter your Address Name");
             contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter your City Name");
             contact.City = Console.ReadLine();
+            Console.WriteLine("Enter your State Name");
             contact.State = Console.ReadLine();
+            Console.WriteLine("Enter your Zip Code");
             contact.Zip = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter your Phone Number");
             contact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter your Email Id");
             contact.Email = Console.ReadLine();
             address.Add(contact);
             Display();
         }
         public void Edit(string name)
         {
-            Create();
-            if (contact.FirstName == name || contact.LastName == name)
+            foreach (var contact in address)
             {
-                Console.WriteLine("\nWhat do you want to edit ?");
-                Console.WriteLine("1. Address \n2. City \n3. State \n4. Zip Code \n5. Phone Number \n6. Email Id \n");
-                int option = Convert.ToInt32(Console.ReadLine());
-                switch (option)
+                if (contact.FirstName == name || contact.LastName == name)
                 {
-                    case 1:
-                        Console.WriteLine("Enter your updated Address!");
-                        contact.Address = Console.ReadLine();
-                        break;
-                    case 2:
-                        Console.WriteLine("Enter your updated City!");
-                        contact.City = Console.ReadLine();
-                        break;
-                    case 3:
-                        Console.WriteLine("Enter your updated State!");
-                        contact.State = Console.ReadLine();
-                        break;
-                    case 4:
-                        Console.WriteLine("Enter your updated Zip code!");
-                        contact.Zip = Convert.ToInt64(Console.ReadLine());
-                        break;
-                    case 5:
-                        Console.WriteLine("Enter your updated Phone Nummber!");
-                        contact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
-                        break;
-                    case 6:
-                        Console.WriteLine("Enter your updated Email Id!");
-                        contact.Address = Console.ReadLine();
-                        break;
-                    default:
-                        Console.WriteLine("Try Again");
-                        break;
+                    Console.WriteLine("\nWhat do you want to edit ?");
+                    Console.WriteLine("1. Address \n2. City \n3. State \n4. Zip Code \n5. Phone Number \n6. Email Id \n");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter your updated Address!");
+                            contact.Address = Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter your updated City!");
+                            contact.City = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter your updated State!");
+                            contact.State = Console.ReadLine();
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter your updated Zip code!");
+                            contact.Zip = Convert.ToInt64(Console.ReadLine());
+                            break;
+                        case 5:
+                            Console.WriteLine("Enter your updated Phone Nummber!");
+                            contact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+                            break;
+                        case 6:
+                            Console.WriteLine("Enter your updated Email Id!");
+                            contact.Address = Console.ReadLine();
+                            break;
+                        default:
+                            Console.WriteLine("Try Again");
+                            break;
+                    }
+                    Display();
                 }
-                Display();
             }
         }
-        public void Display()
+        public void Delete(string name)
         {
-            Console.WriteLine("\nFirst Name - " + contact.FirstName + "\nLast Name - " + contact.LastName + "\nAddress - " + contact.Address +
-                "\nCity Name - " + contact.City + "\nState Name - " + contact.State + "\nZip Code - " + contact.Zip +
-                "\nPhone Number - " + contact.PhoneNumber + "\nEmail ID - " + contact.Email);
+            if (contact.FirstName == name || contact.LastName == name)
+            {
+                address.Remove(contact);
+            }
+            Console.WriteLine("Contact Deleted Successfully");
         }
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -12,23 +12,23 @@ namespace AddressBook
                 AddressBookMain create = new AddressBookMain();
                 Console.WriteLine("Welcome to Address Book Problem Statement\n");
                 Console.WriteLine("Enter your Choice");
-                Console.WriteLine("1. Create a new Contact \n2. Edit a Contact \n3. Exit \n");
+                Console.WriteLine("1. Create a new Contact \n2. Edit a Contact \n3. Delete a Contact \n4. Exit \n");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
                         Console.WriteLine("\nEnter your details :-\n");
-                        Console.WriteLine("First Name - " + "\n" + "Last Name - " + "\n" + "Address - " + "\n" +
-                                        "City Name - " + "\n" + "State Name - " + "\n" + "Zip Code - " + "\n" +
-                                        "Phone Number - " + "\n" + "Email ID - ");
                         create.Create();
                         break;
                     case 2:
                         Console.WriteLine("\nEnter your details :-\n");
-                        Console.WriteLine("First Name - " + "\n" + "Last Name - " + "\n" + "Address - " + "\n" +
-                                        "City Name - " + "\n" + "State Name - " + "\n" + "Zip Code - " + "\n" +
-                                        "Phone Number - " + "\n" + "Email ID - ");
                         create.Edit("Lalit");
+                        break;
+                    case 3:
+                        Console.WriteLine("\nEnter your details :-\n");
+
+                        Console.WriteLine("\nEnter name whose detail you want to delete :-\n");
+                        create.Delete("Lalit");
                         break;
                     default:
                         Console.WriteLine("Try Again");
